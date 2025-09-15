1f4c312d63469452bd8a19623bbb610fb59040e3
Dyeggo Bezerra <dyeggo0102@live.com>
2025-09-15 01:35:29 -0300

    Update V2__insert_initial_data.sql

--- BACK-END/src/main/resources/db/migration/V2__insert_initial_data.sql
+++ BACK-END/src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -1,4 +1,8 @@
 INSERT INTO foods (titulo, img, preco) VALUES
 ('Pizza Calabresa', 'https://anamariareceitas.com.br/wp-content/uploads/2024/08/pizza-de-calabresa_1723632793345-768x432.jpg', 35),
 ('Hambúrguer Artesanal', 'https://www.sabornamesa.com.br/media/k2/items/cache/bf1e20a4462b71e3cc4cece2a8c96ac8_XL.jpg', 25),
+('Batata Frita', 'https://gastronomiacarioca.zonasul.com.br/wp-content/uploads/2023/05/batata_frita_destaque_ilustrativo_zona_sul.jpg', 12),
+('Esfirra', 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2022/07/04/688222603-esfirra-legumes-ricota-1-768x512.jpg', 2),
+('Coxinha', 'https://canaldareceita.com.br/wp-content/uploads/2024/09/COXINHA-FIT.jpg', 7),
+('Empada', 'https://www.sabornamesa.com.br/media/k2/items/cache/24c01e452493eba0f9e741ef09a2d61a_XL.jpg', 5),
 ('Tapioca Nordestina', 'https://claudia.abril.com.br/wp-content/uploads/2020/02/receita-tapioca-carne-de-sol.jpg?quality=70&strip=info&w=620', 15);
